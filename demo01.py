1c0ace85520b58fd14cfc54117bd1e4333a945ba
chengchenghaha <chengchenghahaa@foxmail.com>
2020-11-27 18:02:55 +0800

    222

--- demo01.py
+++ demo01.py
@@ -1,1 +1,7 @@
-print("hello python")
+# print("hello python")
+name = input("请输入姓名：")
+age = input("请输入年龄：")
+sex = input("请输入性别：")
+userinfo = {}
+userinfo.update(name=name,age=age,sex=sex)
+print(userinfo)
